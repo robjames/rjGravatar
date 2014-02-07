52a66088a406bda77a002b935a8e100296a4ad6a
Rob James <rob@creativeblob.com>
2014-02-07 15:43:09 +0000

    Initial commit
    v1

--- README.md
+++ README.md
@@ -1,4 +1,17 @@
 rjGravatar
 ==========
 
-AngularJs directive to show a Gravatar image from an email
+AngularJs module to show a Gravatar image from an email address
+
+
+##Demo
+
+[http://htmlpreview.github.io/?https://raw.github.com/robjames/rjTickbox/master/lib/index.html](http://htmlpreview.github.io/?https://raw.github.com/robjames/rjGravatar/master/lib/index.html)
+
+##Usage
+
+**See example html in lib folder**
+
+But essentially, anything from `<img rj-gravatar="rob@inmeres.com" />` to this `<img rj-gravatar="rob@inmeres.com" size="512" default="mm" force="y" rating="pg" />` and anything in between.
+
+Usually you would want `<img rj-gravatar="rob@inmeres.com" size="100" default="mm" />` or similar.
---
+++ rjGravatar.js
@@ -0,0 +1,38 @@
+/* global angular *//* https://github.com/robjames/rjGravatar */
+	angular.module('rjGravatar', [])
+	.run(function(){
+        var md5 = document.createElement('script'); md5.type = 'text/javascript'; md5.async = true;
+        md5.src = 'https://crypto-js.googlecode.com/svn/tags/3.1.2/build/rollups/md5.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(md5, s);
+	})
+	.directive('rjGravatar', [function() {
+		return {
+			restrict:'A',
+			link: function(scope, element, attrs){
+				function getAttr(str, attr, def){
+					var val = (!!attrs[attr]) ? attrs[attr].replace(/\s/, '') : def || '';
+					return (!val) ? '' : '&' + str + '=' + val;
+				}
+				
+				var email = attrs['rjGravatar'];
+				if(!email) return;
+				
+				var size = getAttr('s','size');
+				var defaultImage = encodeURI(getAttr('d','default'));
+				var forceDefaultImage = getAttr('f','force');
+				var rating = getAttr('r','rating');			
+				
+				var options = size+defaultImage+forceDefaultImage+rating;
+				options = options.replace('&', '?');
+				
+				(function waiter(){
+					if(!window.CryptoJS){ return setTimeout(waiter, 37); }
+
+					var hash = CryptoJS.MD5(email);
+					var src = 'https://secure.gravatar.com/avatar/'+hash.toString()+'/'+options;
+					element.attr('src', src);
+				}());				
+			}
+		}
+	}]);
